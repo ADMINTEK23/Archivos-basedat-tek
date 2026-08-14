b48f98ef5b233cbfc9bfaea2550aba099d0120b5
ADMINTEK23 <tekoesrlcv@gmail.com>
2026-08-14 12:02:13 -0600

    Fix DATABASE_URL key in db_utils.py
    CAMBIO DE COMO ENTRO A LOS SECRETS CON COOLIFY

--- db_utils.py
+++ db_utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sqlalchemy import create_engine, text
 
 # OBTENER URL DE LOS SECRETOS SEGUROS
-DATABASE_URL = st.secrets["database"]["url"]
+DATABASE_URL = st.secrets["DATABASE_URL"]
 
 # 1. OPTIMIZACIÓN: Caching de recursos con verificación de estado activa
 @st.cache_resource
