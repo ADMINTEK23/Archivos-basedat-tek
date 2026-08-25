115516b4bdca1271feaa8bef059d520c10c87e82
ADMINTEK23 <tekoesrlcv@gmail.com>
2026-08-25 11:00:31 -0600

    Corregir db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -1,9 +1,10 @@
+import os
 import streamlit as st
 import pandas as pd
 from sqlalchemy import create_engine, text
 
 # OBTENER URL DE LOS SECRETOS SEGUROS
-DATABASE_URL = st.secrets["database"]["url"]
+DATABASE_URL = os.getenv("DATABASE_URL") or st.secrets.get("DATABASE_URL")
 
 # 1. OPTIMIZACIÓN: Caching de recursos con verificación de estado activa
 @st.cache_resource
